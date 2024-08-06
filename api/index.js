423a6050116ab6f300aa3d09b770ddf39ceec505
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-08-06 12:02:53 +0400

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
-const api = express();
-api.listen(3000, () => {
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
