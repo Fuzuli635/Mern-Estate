eb7f2a127a932c2f4cc40f9ed5fe25879c443885
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-08-09 23:57:03 +0400

    Created SignUp UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcrypt";
-import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto ">
+      <h1 className="text-3xl text-center font-bold my-7 ">SignUp</h1>
+      <form className="flex flex-col gap-4 ">
+        <input
+          className="p-3 border rounded-lg outline-none "
+          type="text"
+          placeholder="username"
+          id="username"
+        />
+        <input
+          className="p-3 border rounded-lg outline-none "
+          type="email"
+          placeholder="email"
+          id="email"
+        />
+        <input
+          className="p-3 border rounded-lg outline-none "
+          type="password"
+          placeholder="password"
+          id="password"
+        />
+        <button className="bg-blue-700 text-white p-3 font-semibold rounded-lg  hover:opacity-85 transition-opacity disabled:opacity-45 ">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 ">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500 ">Sign-in</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
-
