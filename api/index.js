8d7883076c4ce386a82bd6dba943b00993eec640
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-08-09 12:43:01 +0400

    Created Signup route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -12,7 +13,9 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
