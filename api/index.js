91e91a3c2f80ea1ec4e84a926f5a3abe2d661658
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-09-05 19:27:11 +0400

    Create Listing API Route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
+import listingRouter from "./routes/listing.route.js";
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -21,6 +22,7 @@ app.listen(3000, () => {
 });
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
