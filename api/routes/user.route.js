2436a6e3bb7fce10cc18187929cc13354976270e
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-09-08 12:13:05 +0400

    Complate getUser Listings API routes Done

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,10 +3,12 @@ import {
   deleteUser,
   test,
   updateUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 export default router;
