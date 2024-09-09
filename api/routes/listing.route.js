95d4bc11ec18cf7680446f8e761a22878f909e4d
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-09-09 13:07:17 +0400

    Complate update lsting funcionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,10 +4,12 @@ import {
   createListing,
   deleteListing,
   updateListing,
+  getListing,
 } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 export default router;
