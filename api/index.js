c5bf20c6554914eeed71bbfbe9c4f203c5b63df9
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-09-12 10:59:14 +0400

    complete

--- api/index.js
+++ api/index.js
@@ -3,28 +3,36 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 dotenv.config();
-import listingRouter from "./routes/listing.route.js";
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
   });
-   const __dirname = path.resolve();
+
+const __dirname = path.resolve();
+
 const app = express();
+
 app.use(express.json());
+
 app.use(cookieParser());
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!!!");
+  console.log("Server is running on port 3000!");
 });
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
